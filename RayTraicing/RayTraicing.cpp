f33e351130249eb30b444bdd911537775de7bfe6
Andres Felipe Mendez <fmendez1212@gmail.com>
2017-05-02 08:09:54 -0500

    antialiasing

--- RayTraicing/RayTraicing.cpp
+++ RayTraicing/RayTraicing.cpp
@@ -1,10 +1,11 @@
 #include <sstream>
 #include <fstream>
 #include <float.h>
+#include <random>
 #include "stdafx.h"
 #include "sphere.h"
 #include "hitable_list.h"
-
+#include "camera.h"
 
 vec3 color(const ray& r, hitable *world) {
 	hit_record rec;
@@ -22,25 +23,33 @@ int main()
 {
 	int nx = 200;
 	int ny = 100;
+	int ns = 100;
 	std::stringstream img;
+	std::default_random_engine generator;
+	std::uniform_real_distribution<float> distribution(0, 1);
+	int dice_roll = distribution(generator);
+
 	img << "P3\n" << nx << " " << ny << "\n255\n";
-	vec3 lower_left_corner(-2.0, -1.0, -1.0);
-	vec3 horizontal(4.0, 0.0, 0.0);
-	vec3 vertical(0.0, 2.0, 0.0);
-	vec3 origin(0.0, 0.0, 0.0);
+	
 	hitable *list[2];
 	list[0] = new sphere(vec3(0, 0, -1), 0.5);
 	list[1] = new sphere(vec3(0, -100.5, -1), 100);
 	hitable *world = new hitable_list(list, 2);
+	camera cam;
 	for (int j = ny-1; j>= 0; j--)
 	{
 		for (int i = 0; i < nx; i++)
 		{
-			float u = float(i) / float(nx);
-			float v = float(j) / float(ny);
-			ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-			vec3 p = r.point_at_paramteter(2.0);
-			vec3 col = color(r, world);
+			vec3 col(0, 0, 0);
+			for (int s = 0; s< ns; s++)
+			{
+				float u = float(i + distribution(generator)) / float(nx);
+				float v = float(j + distribution(generator)) / float(ny);
+				ray r = cam.get_ray(u,v);
+				vec3 p = r.point_at_paramteter(2.0);
+				col += color(r, world);
+			}
+			col /= float(ns);
 			int ir = int(255.99*col[0]);
 			int ig = int(255.99*col[1]);
 			int ib = int(255.99*col[2]);
@@ -48,7 +57,7 @@ int main()
 		}
 	}
 
-	std::ofstream file("hitable.ppm", std::ofstream::out);
+	std::ofstream file("antialiased.ppm", std::ofstream::out);
 	file.clear();
 	file << img.rdbuf();
 	file.close();
