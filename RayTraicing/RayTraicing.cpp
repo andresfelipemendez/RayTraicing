22f9b39a9b0f776a7cf934278f0afadc594d2bf7
Andres Felipe Mendez <fmendez1212@gmail.com>
2017-05-02 09:29:51 -0500

    diffiuse

--- RayTraicing/RayTraicing.cpp
+++ RayTraicing/RayTraicing.cpp
@@ -6,11 +6,26 @@
 #include "sphere.h"
 #include "hitable_list.h"
 #include "camera.h"
+#include <stdlib.h>
+#include <time.h>  
+
+float random() {
+	return static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
+}
+
+vec3 random_in_unit_sphere() {
+	vec3 p;
+	do {
+		p = 2.0 * vec3(random(), random(), random()) - vec3(1, 1, 1);
+	} while (p.squared_length() >= 1.0);
+	return p;
+}
 
 vec3 color(const ray& r, hitable *world) {
 	hit_record rec;
-	if (world->hit(r, 0.0, FLT_MAX, rec)) {
-		return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+	if (world->hit(r, 0.001, FLT_MAX_10_EXP, rec)) {
+		vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+		return 0.5 * color(ray(rec.p, target-rec.p), world);
 	}
 	else {
 		vec3 unit_direction = unit_vector(r.direction());
@@ -24,10 +39,8 @@ int main()
 	int nx = 200;
 	int ny = 100;
 	int ns = 100;
+	srand(time(NULL));
 	std::stringstream img;
-	std::default_random_engine generator;
-	std::uniform_real_distribution<float> distribution(0, 1);
-	int dice_roll = distribution(generator);
 
 	img << "P3\n" << nx << " " << ny << "\n255\n";
 	
@@ -43,13 +56,14 @@ int main()
 			vec3 col(0, 0, 0);
 			for (int s = 0; s< ns; s++)
 			{
-				float u = float(i + distribution(generator)) / float(nx);
-				float v = float(j + distribution(generator)) / float(ny);
+				float u = float(i + random()) / float(nx);
+				float v = float(j + random()) / float(ny);
 				ray r = cam.get_ray(u,v);
 				vec3 p = r.point_at_paramteter(2.0);
 				col += color(r, world);
 			}
 			col /= float(ns);
+			col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
 			int ir = int(255.99*col[0]);
 			int ig = int(255.99*col[1]);
 			int ib = int(255.99*col[2]);
@@ -57,7 +71,7 @@ int main()
 		}
 	}
 
-	std::ofstream file("antialiased.ppm", std::ofstream::out);
+	std::ofstream file("diffused light grey.ppm", std::ofstream::out);
 	file.clear();
 	file << img.rdbuf();
 	file.close();
