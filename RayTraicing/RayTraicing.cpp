d806f79ae42ab4e1f04e71e300d602e3b059cfe5
Andres Felipe Mendez <fmendez1212@gmail.com>
2017-05-02 10:41:52 -0500

    materials

--- RayTraicing/RayTraicing.cpp
+++ RayTraicing/RayTraicing.cpp
@@ -6,26 +6,22 @@
 #include "sphere.h"
 #include "hitable_list.h"
 #include "camera.h"
+#include "material.h"
 #include <stdlib.h>
 #include <time.h>  
+#include "hitable.h"
 
-float random() {
-	return static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
-}
-
-vec3 random_in_unit_sphere() {
-	vec3 p;
-	do {
-		p = 2.0 * vec3(random(), random(), random()) - vec3(1, 1, 1);
-	} while (p.squared_length() >= 1.0);
-	return p;
-}
-
-vec3 color(const ray& r, hitable *world) {
+vec3 color(const ray& r, hitable *world, int depth) {
 	hit_record rec;
-	if (world->hit(r, 0.001, FLT_MAX_10_EXP, rec)) {
-		vec3 target = rec.p + rec.normal + random_in_unit_sphere();
-		return 0.5 * color(ray(rec.p, target-rec.p), world);
+	if (world->hit(r, 0.001, FLT_MAX, rec)) {
+		ray scattered;
+		vec3 attenuation;
+		if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+			return attenuation*color(scattered, world, depth + 1);
+		}
+		else {
+			return vec3(0, 0, 0);
+		}
 	}
 	else {
 		vec3 unit_direction = unit_vector(r.direction());
@@ -44,10 +40,12 @@ int main()
 
 	img << "P3\n" << nx << " " << ny << "\n255\n";
 	
-	hitable *list[2];
-	list[0] = new sphere(vec3(0, 0, -1), 0.5);
-	list[1] = new sphere(vec3(0, -100.5, -1), 100);
-	hitable *world = new hitable_list(list, 2);
+	hitable *list[4];
+	list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8,0.3,0.3)));
+	list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8,0.8,0.0)));
+	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2)));
+	list[3] = new sphere(vec3(-1, 0, -1), 0.5, new metal(vec3(0.8, 0.8, 0.8)));
+	hitable *world = new hitable_list(list, 4);
 	camera cam;
 	for (int j = ny-1; j>= 0; j--)
 	{
@@ -60,7 +58,7 @@ int main()
 				float v = float(j + random()) / float(ny);
 				ray r = cam.get_ray(u,v);
 				vec3 p = r.point_at_paramteter(2.0);
-				col += color(r, world);
+				col += color(r, world, 0);
 			}
 			col /= float(ns);
 			col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
@@ -71,7 +69,7 @@ int main()
 		}
 	}
 
-	std::ofstream file("diffused light grey.ppm", std::ofstream::out);
+	std::ofstream file("materials.ppm", std::ofstream::out);
 	file.clear();
 	file << img.rdbuf();
 	file.close();
